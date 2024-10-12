5bbd98c91dd8081a320f181a6b4a84fbaf9b20fe
John Triantafyllakis <john.3fyl@gmail.com>
2024-10-12 12:34:33 +0300

    Simplified script for initialization of db

--- docker/database/init-db.sh
+++ docker/database/init-db.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-set -e
-
-# Wait for MySQL to start
-echo "Waiting for MySQL to start..."
-until mysql -hlocalhost -uroot -p"$MYSQL_ROOT_PASSWORD" -e "SELECT 1;" > /dev/null 2>&1; do
-  sleep 1
-done
-
-echo "MySQL is up - executing scripts"
-# Execute SQL commands
 mysql --protocol=socket -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOSQL
   CREATE DATABASE IF NOT EXISTS $SCHEMA_KC;
   CREATE USER IF NOT EXISTS '$SCHEMA_KC_USER'@'%' IDENTIFIED BY '$SCHEMA_KC_PASSWORD';
